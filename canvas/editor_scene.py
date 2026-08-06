1b07f1f4018dfeef254e7d5ba985cc9a1986b980
pc-wsl-sonu <sonukumarzeroone@gmail.com>
2026-08-05 20:38:58 -0700

    [add] grid matrix in editor scene

--- canvas/editor_scene.py
+++ canvas/editor_scene.py
@@ -1,6 +1,8 @@
 
 from PySide6.QtWidgets import QGraphicsScene
 from PySide6.QtGui import QColor
+from qtpy.QtCore import QRect, QRectF
+from qtpy.QtGui import QPainter
 
 from canvas.page_item import PageItem
 from utils.constants import MARGIN
@@ -10,8 +12,6 @@ class EditorScene(QGraphicsScene):
 
     def __init__(self) -> None:
         super().__init__()
-
-        self.setBackgroundBrush(QColor(45,45,45))
 
         self._page = PageItem()
         self.addItem(self._page)
@@ -28,3 +28,31 @@ class EditorScene(QGraphicsScene):
     @property
     def page(self) -> PageItem:
         return self._page
+    
+    def drawBackground(
+        self, 
+        painter: QPainter, 
+        rect: QRectF | QRect
+    ) -> None:
+        painter.fillRect(
+            rect,
+            QColor(46, 46, 46)
+        )
+        
+        spacing = 20
+        dot_color = QColor(64, 64, 64)
+        painter.setPen(dot_color)
+        
+        left = int(rect.left()) - (int(rect.left()) % spacing)
+        top = int(rect.top()) - (int(rect.top()) % spacing)
+        
+        right = int(rect.right())
+        bottom = int(rect.bottom())
+        
+        x = left
+        while x <= right:
+            y = top
+            while y <= bottom:
+                painter.drawPoint(x, y)
+                y += spacing
+            x += spacing
