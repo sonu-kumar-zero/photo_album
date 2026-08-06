ad5d3df4522a2832ca003796b522732527cfccf4
pc-wsl-sonu <sonukumarzeroone@gmail.com>
2026-08-06 07:33:57 -0700

    [add] canvas item: base class for every editable object

---
+++ items/canvas_item.py
@@ -0,0 +1,86 @@
+from __future__ import annotations
+
+from PySide6.QtCore import QRectF, QUuid
+from PySide6.QtGui import QColor, QPen, QPainter
+from PySide6.QtWidgets import QGraphicsItem, QGraphicsObject
+
+
+class CanvasItem(QGraphicsObject):
+    """
+    Base class for every editable object on a page.
+    """
+    """
+    Future responsibilities
+
+    - Selection
+    - Hover
+    - Resize Handles
+    - Rotation Handle
+    - UUID
+    - Serialization
+    - Context Menu
+    - Locking
+    - Visibility
+    """
+
+    def __init__(
+        self,
+        rect: QRectF,
+        parent: QGraphicsItem | None = None,
+    ) -> None:
+        super().__init__(parent)
+
+        self._id = QUuid.createUuid()
+        self._rect = QRectF(rect)
+
+        self.setFlag(
+            QGraphicsItem.GraphicsItemFlag.ItemIsSelectable,
+            True,
+        )
+
+        self.setFlag(
+            QGraphicsItem.GraphicsItemFlag.ItemIsMovable,
+            True,
+        )
+
+        self.setFlag(
+            QGraphicsItem.GraphicsItemFlag.ItemSendsGeometryChanges,
+            True,
+        )
+
+        self.setAcceptHoverEvents(True)
+
+        self._normal_pen = QPen(QColor(180, 180, 180), 1)
+        self._selected_pen = QPen(QColor(0, 120, 215), 2)
+
+    @property
+    def id(self) -> QUuid:
+        return self._id
+
+    def rect(self) -> QRectF:
+        return QRectF(self._rect)
+
+    def setRect(self, rect: QRectF) -> None:
+        self.prepareGeometryChange()
+        self._rect = QRectF(rect)
+        self.update()
+
+    def boundingRect(self) -> QRectF:
+        return self._rect
+
+    def paint(
+        self,
+        painter: QPainter,
+        option,
+        widget=None,
+    ) -> None:
+        raise NotImplementedError
+    
+    @property
+    def width(self) -> float:
+        return self._rect.width()
+    
+    @property
+    def height(self) -> float:
+        return self._rect.height()
+    
